ef964ccecfb75ac6a13e4c86f1d366f46ea54657
Dennis Jefferson <dennismjefferson@gmail.com>
2019-09-28 09:45:35 -0700

    REMOVE TODO FUNC

--- js/app.js
+++ js/app.js
@@ -1,35 +1,4 @@
 (function (window) {
 	'use strict';
-  const todoInput = document.querySelectorAll('.new-todo')[0];
-  const todoList = document.querySelectorAll('.todo-list')[0];
-
-	// Your starting point. Enjoy the ride!
-
-  function todoTemplate(value) {
-    return `
-       <div class="view">
-         <input class="toggle" type="checkbox" {{checked}}>
-         <label>${value}</label>
-         <button class="destroy"></button>
-       </div>
-    `;
-
-  }
-
-  function createTodo(value) {
-    const newTodo = document.createElement('li');
-
-    newTodo.innerHTML = todoTemplate(value);
-
-    todoList.append(newTodo)
-  }
-
-  function handleEvent(e) {
-    if (e.code === 'Enter' && e.target.value) {
-      createTodo(e.target.value)
-    }
-  }
-
-  todoInput.addEventListener('keyup', handleEvent)
 
 })(window);
